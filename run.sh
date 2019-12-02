91f1bed9ff1aade376c8bd0642256f3ab4beb24f
Hack4day <hieupm5@fpt.com.vn>
2019-12-02 16:31:50 +0700

    second_stable

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo docker build --tag=<tên image> .
-
-sudo docker run --rm -it --gpus=all --network=host --memory=6000M -d -v ~/<đường dẫn nối volume>:/root/catkin_ws/src/ --name <tên đội> <tên image> bash
